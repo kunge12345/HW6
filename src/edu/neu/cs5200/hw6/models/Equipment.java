ad7c469c9644a692d071d1236b9d8698436f04b5
kunge12345 <kunliu92@gmail.com>
2015-04-29 18:02:50 -0400

    first commit

---
+++ src/edu/neu/cs5200/hw6/models/Equipment.java
@@ -0,0 +1,75 @@
+package edu.neu.cs5200.hw6.models;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+@Entity
+@XmlRootElement
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class Equipment{
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@XmlAttribute
+	private int id;
+	@XmlAttribute
+	private String name;
+	@XmlAttribute
+	private String brand;
+	@XmlAttribute
+	private String description;
+	@XmlAttribute
+	private double price;
+
+	@ManyToOne
+	@JoinColumn(name="towerId")
+	@XmlTransient
+	private Tower tower;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+		public Tower getTower() {
+		return tower;
+	}
+	public void setTower(Tower tower) {
+		this.tower = tower;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setName(String description) {
+		this.description = description;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	public Equipment(String name,String brand, String description,double price ) {
+		super();
+		this.name = name;
+		this.brand = brand;
+		this.description = description;
+		this.price = price;
+	}
+
+	public Equipment() {
+		super();
+	}
+	
+}
---
+++ src/edu/neu/cs5200/hw6/models/Site.java
@@ -0,0 +1,79 @@
+package edu.neu.cs5200.hw6.models;
+
+import java.util.List;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
+
+@Entity
+@NamedQueries(value = { @NamedQuery(name = "findAllSites", query = "select dir from Site dir") })
+@XmlRootElement
+@XmlAccessorType(value = XmlAccessType.FIELD)
+public class Site {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@XmlAttribute
+	private int id;
+	@XmlAttribute
+	private String name;
+	@XmlAttribute
+	private int latitude;
+	@XmlAttribute
+	private int longitude;
+
+	@OneToMany(mappedBy = "site", cascade = CascadeType.ALL, orphanRemoval = true)
+	@XmlElement(name = "tower")
+	private List<Tower> towers;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setname(String name) {
+		this.name = name;
+	}
+
+	public int getLatitude() {
+		return latitude;
+	}
+
+	public void getLatitude(int latitude) {
+		this.latitude = latitude;
+	}
+
+	public int getLongitude() {
+		return longitude;
+	}
+
+	public void getlongitude(int longitude) {
+		this.longitude = longitude;
+	}
+
+	public List<Tower> getTower() {
+		return towers;
+	}
+
+	public void setTower(List<Tower> towers) {
+		this.towers = towers;
+	}
+
+	public Site(String name, int latitude, int longitude) {
+		super();
+		this.name = name;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public Site() {
+		super();
+	}
+
+}
